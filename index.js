2a29adc2d9a94c409bd1e0c9ef7a091c834d7ecc
Abraham Lin <al684@cornell.edu>
2018-01-04 17:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function shout(string) {
   return string.toUpperCase();
 }
 function whisper(string) {
-  console.log(string.toLowerCase());
+  return string.toLowerCase();
 }
 function logShout(string) {
   console.log(string.toUpperCase());
